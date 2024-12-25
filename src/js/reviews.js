e3826f12e31a29d0c71ab61ce4fed66be1c25976
andriiKrakovskyi <andrii.krakovskyi@gmail.com>
2024-12-25 10:32:55 +0100

    ‘img-svg’

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,41 +1,46 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import axios from "axios";
-import Swiper from "swiper";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import axios from 'axios';
+import Swiper from 'swiper';
 import 'swiper/css';
 
-const apiUrl = "https://portfolio-js.b.goit.study/api/reviews";
+const apiUrl = 'https://portfolio-js.b.goit.study/api/reviews';
 const cardsContainer = document.querySelector('.cards');
 
 async function fetchData() {
-    const { data } = await axios(apiUrl)
-    return data;
+  const { data } = await axios(apiUrl);
+  return data;
 }
 
 fetchData()
-    .then(data => {
-        cardsContainer.insertAdjacentHTML("beforeend", createCards(data));
-    })
-    .catch(error => {
-        let serverError = error;
+  .then(data => {
+    cardsContainer.insertAdjacentHTML('beforeend', createCards(data));
+  })
+  .catch(error => {
+    let serverError = error;
 
-        let observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting && serverError) {
-                    showError("Виникли проблеми з отриманням даних із сервера.");
-                    cardsContainer.insertAdjacentHTML("beforeend", `<p class="not-found"> Not found</p>`)
-                    serverError = null;
-                    observer.disconnect();
-                }
-            });
-        });
+    let observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting && serverError) {
+          showError('Виникли проблеми з отриманням даних із сервера.');
+          cardsContainer.insertAdjacentHTML(
+            'beforeend',
+            `<p class="not-found"> Not found</p>`
+          );
+          serverError = null;
+          observer.disconnect();
+        }
+      });
+    });
 
-        const reviewsSection = document.getElementById('reviews');
-        observer.observe(reviewsSection);
-})
+    const reviewsSection = document.getElementById('reviews');
+    observer.observe(reviewsSection);
+  });
 
 function createCards(arr) {
-    return arr.map(({ author, avatar_url, review }) => `
+  return arr
+    .map(
+      ({ author, avatar_url, review }) => `
     <li class="card swiper-slide">
     <img src="${avatar_url}" alt="Author's photo" class="card-img">
     <div class="card-content">
@@ -43,47 +48,49 @@ function createCards(arr) {
     <p class="common-text"> ${review}</p>
     </div>
     </li>
-    `).join("");
+    `
+    )
+    .join('');
 }
 
 const swiper = new Swiper('.swiper', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  centeredSlidesBounds: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
     },
-    centeredSlidesBounds: true,
-    slidesPerView: 1,
-    spaceBetween: 10,
-    breakpoints: {
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 16
-        },
-        1280: {
-            slidesPerView: 4,
-            spaceBetween: 16
-        }
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 16,
     },
+  },
 });
 
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowRight') {
-        swiper.slideNext();
-    } else if (event.key === 'ArrowLeft') {
-        swiper.slidePrev();
-    } else if (event.key === 'Tab') {
-        event.preventDefault();
-        if (event.shiftKey) {
-            swiper.slidePrev();
-        } else {
-            swiper.slideNext();
-        }
+document.addEventListener('keydown', event => {
+  if (event.key === 'ArrowRight') {
+    swiper.slideNext();
+  } else if (event.key === 'ArrowLeft') {
+    swiper.slidePrev();
+  } else if (event.key === 'Tab') {
+    event.preventDefault();
+    if (event.shiftKey) {
+      swiper.slidePrev();
+    } else {
+      swiper.slideNext();
     }
+  }
 });
 
 function showError(message) {
-    iziToast.error({
-        title: 'Error',
-        message: message,
-    });
+  iziToast.error({
+    title: 'Error',
+    message: message,
+  });
 }
